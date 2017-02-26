132e9ea6ef957c6acf19ef8ad7826f569434aa17
Shahad Mansour <shahad@MacBook-Pro-alkhas-b-Shahad.local>
2017-02-26 17:18:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/shahad/instify/log_in.java
@@ -0,0 +1,18 @@
+package com.example.shahad.instify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class log_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+
+
+
+        };
+    }
+
+
---
+++ app/src/main/java/com/example/shahad/instify/register_buyer.java
@@ -0,0 +1,13 @@
+package com.example.shahad.instify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class register_buyer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_buyer1);
+    }
+}
